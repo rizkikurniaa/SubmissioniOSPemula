168d7c1d9edec72f54f571f9df445855405c0045
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2022-06-27 21:12:12 +0700

    Initial Commit

---
+++ SubmissioniOSPemula/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubmissioniOSPemula
+//
+//  Created by RIZKI on 27/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SubmissioniOSPemula/SubmissioniOSPemulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmissioniOSPemulaApp.swift
+//  SubmissioniOSPemula
+//
+//  Created by RIZKI on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SubmissioniOSPemulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
