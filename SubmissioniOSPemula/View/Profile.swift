3d6d7b4a403298b674b64444203a9c381b6c486d
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2022-06-28 20:42:22 +0700

    Done profile

--- SubmissioniOSPemula/View/Profile.swift
+++ SubmissioniOSPemula/View/Profile.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack(spacing:14){
+                Image("rizki")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: 10)
+                
+                VStack(spacing:5){
+                    Text("Rizki Kurniawan")
+                        .font(.title3)
+                        .bold()
+                    
+                    Text("Android and iOS Developer")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("Hi, I'm Rizki Kurniawan. I am an Android and iOS developer. I like to learn new things every day.")
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+                    .padding(.vertical, 20.0)
+                
+                
+            }
+            .padding(.vertical, 30.0)
+        }
+        .padding()
+        
     }
 }
 
